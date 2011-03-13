387a98085152c01949b28bcbe4a914d3897a0dee
U-SROBO\ehuggins <ehuggins@EHUGGINSLP320GB.srobo.local>
2011-03-13 17:20:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '74f9568a2ce36da2c6917c7c64e947d98e9c1e47aecf9cebfb0a232dc0ce5b4bb4d3f672e79963d24ffd68eaa0c521677e68debeff4dc8480ca6e71e1a8626f9'
